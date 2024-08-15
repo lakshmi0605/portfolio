87f56f99eaa044ed99b59a96a0e1f37d0b05dd73
Lakshmi <you@example.comlakshmipanguluri98@gmail.com>
2024-08-15 10:00:52 -0700

    Added contact form, instructions and styled back cover

--- index.js
+++ index.js
@@ -1,60 +1,3 @@
-// function onClick(event){
-//     let eleId = event.target.id;
-//     if(eleId === ""){
-//         let parent = event.target;
-//         while (parent && parent.tagName !== 'SECTION') {
-//             parent = parent.parentElement;
-//         }
-//         if (parent && parent.tagName === 'SECTION') {
-//             eleId = parent.id;
-//         }
-//     }
-//     const coverEle = document.getElementById(eleId);
-//     let className ="";
-//     let className1 ="";
-//     let className2 ="";
-//     switch(eleId){
-//         case "coverPage":
-//             className1 = "coverClick";
-//             className2 = "coverBottom"
-//             break;
-//         case "homePage":
-//             className1 = "homeClick";
-//             className2 = "homeBottom"
-//             break;
-//         case "aboutPage":
-//             className1 = "aboutClick";
-//             className2 = "aboutBottom";
-//             break;
-//         case "portfolioPage":
-//             className1 = "portfolioClick";
-//             className2 = "portfolioBottom";
-//             break;
-//         case "contactPage":
-//             className1 = "contactClick";
-//             className2 = "contactBottom";
-//             break;
-//         case "endPage":
-//             className1 = "endClick";
-//             break;
-//     }
-//     coverEle.classList.add(className1);
-//     if(className2){
-//         coverEle.addEventListener('transitionend', function(event) {
-//               coverEle.classList.add(className2);
-//           }, { once: true });
-//     } 
-// }
-
-
-// function onClickBack(event){
-//     const eleId = event.target.id;
-//     const parentId = eleId.split("-")[1];
-//     document.getElementById(parentId).classList.toggle("coverClick")
-   
-
-// }
-
 function onClick(event){
     const eleId = event.target.id;
     const [direction, pageId ] = eleId.split("-");
@@ -74,5 +17,20 @@ function onCoverClick(event){
     const coverId = event.target.id;
     const coverEle = document.getElementById(coverId);
     coverEle.classList.add("coverClick");
+}
 
+function onContactSubmit(event){
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Log the input values to the console
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    // Optionally, you can display a confirmation message to the user
+    alert('Form submitted! Check the console for details.');
 }
